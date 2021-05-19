0668da9cd1627bf6f3602ff45136419a6fb3126b
luxihua201010028 <luxihua201010028@163.com>
2021-05-19 23:32:03 +0800

    add 父类和子类

--- class/父类和子类关系，继承.py
+++ class/父类和子类关系，继承.py
@@ -3,7 +3,7 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
-        self.odo = 20
+        self.odo = 0
 
     def get_descriptive_name(self):
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
@@ -19,15 +19,19 @@ class Car:
             print("you can't roll back an odo")
     def increment_odo(self,miles):
         self.odo+=miles
+class Eletricar(Car):
+    '''电动汽车的独到之处'''
+    def __init__(self,make,model,year):
+        '''初始化父类属性'''
+        super().__init__(make,model,year)
+        self.batrery=70
+    def describle_battery(self):
+        '''打印一条描述电瓶容量的消息'''
+        print('my car have a '+str(self.batrery)+'  -kwh battery')
+    def fill_gas_tank(self):
+        print('this car does not need  GAS TANK')
+    
+my_tesla=Eletricar('tesla','model s',2018)
+print(my_tesla.get_descriptive_name())
 
-# my_new_car = Car('audi', 'a4', 2016)
-# my_new_car.year=1000
-# print(my_new_car.get_descriptive_name())
-# my_new_car.update_odo(200)
-# my_new_car.read_odo()
-my_used_car = Car('subaru', 'a4', 2013)
-print(my_used_car.get_descriptive_name())
-my_used_car.update_odo(23500)
-my_used_car.read_odo()
-my_used_car.increment_odo(500)
-my_used_car.read_odo()
+my_tesla.describle_battery()
