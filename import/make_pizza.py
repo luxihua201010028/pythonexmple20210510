6b3b528ddf1cfd50cd852296fecfc4f8bf00b40b
luxihua201010028 <42412379+luxihua201010028@users.noreply.github.com>
2021-05-20 22:35:54 +0800

    delete import

--- import/make_pizza.py
+++
@@ -1,3 +0,0 @@
-import pizza
-pizza.make_pizza(16, 'peppersioni')
-pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
--- import/make_pizza1.py
+++
@@ -1,9 +0,0 @@
-# from pizza import make_pizza
-# import pizza
-# from pizza import make_pizza as mp
-# mp(12, 'pizza1', "分享一个精彩视频《中国1945之重庆风云 第16集》")
-# import pizza as p
-# p.make_pizza(12, 'pizza1', "分享一个精彩视频《中国1945之重庆风云 第16集》")
-
-from pizza import *
-make_pizza(12, 'pizza1', "分享一个精彩视频《中国1945之重庆风云 第16集》")
